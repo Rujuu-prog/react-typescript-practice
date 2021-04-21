5347f54422639450fa2b94bce9f6c8e9b95fc194
Rujuu-prog <n.ujiie2118@gmail.com>
2021-04-22 02:21:06 +0900

    propsの型指定

--- src/Todo.tsx
+++ src/Todo.tsx
@@ -1,10 +1,19 @@
 import React from 'react'
 
-export const Todo = (props: any) => {
-    const {title, userid} = props;
+type TodoType = {
+    userId: number;
+    title: string;
+    //?をつけることで必須ではないことを明示的にかける。completedは渡さなくても良いものという扱いになる
+    completed?: boolean;
+}
+
+export const Todo = (props: TodoType) => {
+    //もしpropsで値が渡ってこない場合の初期値をつけておくcompleted
+    const {title, userId, completed = false} = props;
+    const completeMark = completed? "[完]": "[未]";
     return (
         <div>
-            <p>{`${title}(ユーザー:${userid})`}</p>
+            <p>{`${completeMark} ${title}(ユーザー:${userId})`}</p>
         </div>
     )
 }
